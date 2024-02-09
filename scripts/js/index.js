4bc06f0b1c692618b2d453cba7a13c74829e8fce
Yhumi0210 <buillaura0210@gmail.com>
2024-02-09 11:19:48 +0100

    base code

---
+++ scripts/js/index.js
@@ -0,0 +1,11 @@
+import { recipes } from '../json/recipes.js'
+// Variables
+// const response = await fetch('./scripts/json/recipes.js')
+// const data = await response.json()
+// const recipes = recipes
+console.log(recipes)
+
+function getRecipes() {
+    // je peux directement construire le DOM en ajoutant les recettes en bouclant pour chaque
+    
+}
