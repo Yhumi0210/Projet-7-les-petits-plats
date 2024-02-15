2be9395c2c5200a1763eec9b637e34d41cf44448
Yhumi0210 <buillaura0210@gmail.com>
2024-02-15 16:19:15 +0100

    fonction de recherche method array

--- scripts/js/functions/search.js
+++ scripts/js/functions/search.js
@@ -1,22 +1,36 @@
 import { recipes } from '../dataBase/recipes.js'
-export function getResultSearch()
-{
-    const searchBar = document.getElementById('recipe-search')
-    searchBar.addEventListener('input', function() {
-        const searchValue = searchBar.value.toLowerCase()
-        const filteredRecipes = recipes.filter(recipe => {
-            return recipe.name.toLowerCase().includes(searchValue) || recipe.ingredients.some(ingredient => ingredient.ingredient.toLowerCase().includes(searchValue))
-        })
-        renderFilteredRecipes(filteredRecipes)
+import { recipeTemplate } from '../templates/recipeFactory.js'
+
+// Fonction pour filtrer les recettes en fonction de la recherche
+function searchRecipes(searchText) {
+    const searchFilteredRecipes = recipes.filter(recipe => {
+        return recipe.name.toLowerCase().includes(searchText.toLowerCase()) || recipe.ingredients.some(ingredient => ingredient.ingredient.toLowerCase().includes(searchText.toLowerCase()))
+    })
+    return searchFilteredRecipes
+}
+
+// Fonction pour mettre à jour l'affichage des recettes
+function updateRecipeDisplay(filteredRecipes) {
+    const cardRecipe = document.querySelector('.hero')
+    cardRecipe.innerHTML = ''
+
+    for (const recipe of filteredRecipes) {
+        const recipeModel = recipeTemplate()
+        const recipeDOM = recipeModel.getRecipeDOM(recipe)
+        cardRecipe.appendChild(recipeDOM)
+    }
+}
+
+// Fonction pour gérer les événements de saisie dans la barre de recherche
+function searchInput() {
+    const searchInput = document.getElementById('recipe-search')
+
+    searchInput.addEventListener('input', () => {
+        const searchText = searchInput.value
+        const filteredRecipes = searchRecipes(searchText)
+        updateRecipeDisplay(filteredRecipes)
     })
 }
 
-function renderFilteredRecipes(filteredRecipes) {
-    const heroContainer = document.querySelector('.hero')
-    heroContainer.innerHTML = '' // Clear previous recipes
-    filteredRecipes.forEach(recipe => {
-        // Render each filtered recipe similarly to the original rendering logic
-        // You can reuse the logic from your getRecipes() function
-        // ...
-    })
-}
+// Appel de la fonction pour gérer les événements de saisie
+searchInput()
